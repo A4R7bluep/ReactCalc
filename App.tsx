2ac01e1b2101f85bd851e696081cbfb273adcce1
Ongsosr Ongsosr <a4r7bluep@gmail.com>
2023-01-18 12:21:43 -0500

    custom calcbutton

--- App.tsx
+++ App.tsx
@@ -10,41 +10,50 @@
 
 import React, { type PropsWithChildren, useState } from 'react';
 import {
-	SafeAreaView,
-	ScrollView,
-	StatusBar,
-	StyleSheet,
-	Text,
-	useColorScheme,
-	View,
-	Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
 } from 'react-native';
 
-var calcStr = "calcStr";
-
-// function updateCalcStr(mod: string) {
-// 	calcStr += mod;
+// const text = (props: { num: string }) => {
+//   return (
+//     <Text>{props.num}</Text>
+//   )
 // }
 
-const CalcButton = (props: { number: string; }) => {
-	return (
-		<Button onPress={() => calcStr = props.number} title={props.number}></Button>
-	);
+labelVal = '';
+
+const NumButton = (props: { num: string }) => {
+  const [textVal, setValue] = useState('');
+
+  function addToCalcStr(props: { adder: string }) {
+    setValue(textVal + props.adder);
+    labelVal = textVal;
+  }
+
+  return (
+    <Button onPress={() => addToCalcStr({ adder: props.num })} title={props.num} />
+  )
 }
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>{calcStr}</Text>
-			<CalcButton number="1" ></CalcButton>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>{labelVal}</Text>
+      <NumButton num="1" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
